72f216d3169ba71bac5484fe6e4bb4640edb4baf
Igor Chernyshev <igor.chernyshev@cygrpdal.local>
2020-03-21 01:39:19 -0500

    faker data

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ for (let i = 0; i < 20; i++) {
     name: faker.commerce.productName(),
     color: faker.commerce.color(),
     price: faker.commerce.price(),
-    quantity: Math.floor(Math.random() * 5),
+    numberInStock: Math.floor(Math.random() * 5),
     isInStock: false
   };
 
-  product.isInStock = product.quantity > 0;
+  product.isInStock = product.numberInStock > 0;
 
   products.push(product);
 }
