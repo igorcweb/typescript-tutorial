f13866927ee575dba8ccd46c5cd97f310adf9389
Igor Chernyshev <igor.chernyshev@cygrpdal.local>
2020-03-21 03:24:21 -0500

    es6

--- index.js
+++ index.js
@@ -1,26 +1,20 @@
-const faker = require("faker");
-const fs = require("fs");
-
+import * as faker from "faker";
+import * as fs from "fs";
 const products = [];
-
 for (let i = 0; i < 20; i++) {
-  const product = {
-    name: faker.commerce.productName(),
-    color: faker.commerce.color(),
-    price: faker.commerce.price(),
-    numberInStock: Math.floor(Math.random() * 5),
-    isInStock: false
-  };
-
-  product.isInStock = product.numberInStock > 0;
-
-  products.push(product);
+    const product = {
+        name: faker.commerce.productName(),
+        color: faker.commerce.color(),
+        price: faker.commerce.price(),
+        numberInStock: Math.floor(Math.random() * 5),
+        isInStock: false
+    };
+    product.isInStock = product.numberInStock > 0;
+    products.push(product);
 }
-
-let jsonProducts = JSON.stringify(products);
-
+const jsonProducts = JSON.stringify(products);
 fs.writeFile("products.json", jsonProducts, err => {
-  if (err) {
-    console.log(err);
-  }
+    if (err) {
+        console.log(err);
+    }
 });
---
+++ index.ts
@@ -0,0 +1,26 @@
+import * as faker from "faker";
+import * as fs from "fs";
+
+const products = [];
+
+for (let i = 0; i < 20; i++) {
+  const product = {
+    name: faker.commerce.productName(),
+    color: faker.commerce.color(),
+    price: faker.commerce.price(),
+    numberInStock: Math.floor(Math.random() * 5),
+    isInStock: false
+  };
+
+  product.isInStock = product.numberInStock > 0;
+
+  products.push(product);
+}
+
+const jsonProducts = JSON.stringify(products);
+
+fs.writeFile("products.json", jsonProducts, err => {
+  if (err) {
+    console.log(err);
+  }
+});
